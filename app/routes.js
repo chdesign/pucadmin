5216dd55411d962e3766d916c9f7cdc10c06f50e
Chris Hanson <chrisphanson@outlook.com>
2018-01-26 15:55:49 +0000

    Updated flow ready for UR on monday

--- app/routes.js
+++ app/routes.js
@@ -9,11 +9,34 @@ router.get('/', function (req, res) {
 
 // Add your routes here - above the module.exports line
 
+
+router.get('/admin', function (req, res) {
+  res.redirect('/admin/plan-types')
+})
+
 router.post('/admin/plan-types/create/todo/main-content-preview', function (req, res){
 
   req.session.data['todoRendered'] = marked(req.session.data['todo-main-content'])
 
   res.redirect('/admin/plan-types/create/todo/main-content-preview')
+
+})
+
+
+router.post('/admin/plan-types/create/todo/stepped/main-content-preview', function (req, res){
+
+  req.session.data['todoRendered'] = marked(req.session.data['todo-main-content'])
+
+  res.redirect('/admin/plan-types/create/todo/stepped/main-content-preview')
+
+})
+
+
+router.post('/admin/plan-types/create/todo/stepped/add-task-preview', function (req, res){
+
+  req.session.data['taskRendered'] = marked(req.session.data['task-content'])
+
+  res.redirect('/admin/plan-types/create/todo/stepped/add-task-preview')
 
 })
 
