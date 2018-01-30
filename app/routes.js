f8ff815b94934e1f16dec6837c6e53a3df83bd8c
Chris Hanson <chrisphanson@outlook.com>
2018-01-30 12:55:40 +0000

    Updated flow based on feedback Added tag management to admin

--- app/routes.js
+++ app/routes.js
@@ -16,6 +16,18 @@ router.get('/admin', function (req, res) {
 
 
 
+router.get('/assign', function (req, res) {
+  var assign = req.session.data['assign']
+
+  if ( assign == 'new' ) {
+    res.redirect('/admin/sites/addsite')
+  } else {
+    res.redirect('/admin/sites/upgrade')
+  }
+})
+
+
+
 
 
 router.get('/deletedraftplan', function (req, res) {
@@ -25,6 +37,16 @@ router.get('/deletedraftplan', function (req, res) {
 
 router.get('/canceltodo', function (req, res) {
   delete req.session.data['todo-title']
+  delete req.session.data['todo-start']
+  delete req.session.data['todo-end']
+  delete req.session.data['todo-priority']
+  delete req.session.data['todo-main-lede']
+  delete req.session.data['todo-main-content']
+  delete req.session.data['task-content']
+  delete req.session.data['link-title']
+  delete req.session.data['link-url']
+  delete req.session.data['todo-tags']
+  delete req.session.data['release-content']
   res.redirect('/admin/plan-types/create/draft')
 })
 
